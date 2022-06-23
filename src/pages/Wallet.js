bbfd53feb8fc19dcfa16a6722fde27049b2d77b6
lucaspaesj <lpjanuzi@gmail.com>
2022-06-23 15:11:16 -0300

    requisito 5

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -5,16 +5,107 @@ import Header from '../components/Header';
 import { fetchApi } from '../actions';
 
 class Wallet extends React.Component {
+  state = {
+    valueInput: '',
+    descriptionInput: '',
+    currencieInput: 'USD',
+    methodInput: 'Dinheiro',
+    tagInput: 'Alimentação',
+  }
+
   async componentDidMount() {
     const { dispatch } = this.props;
     await dispatch(fetchApi());
   }
 
+  onChangeInput = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const { currencies } = this.props;
+    const {
+      valueInput,
+      descriptionInput,
+      currencieInput,
+      methodInput,
+      tagInput,
+    } = this.state;
     return (
       <>
         <Header />
-        <div>TrybeWallet</div>
+        <form>
+          <label htmlFor="value-input">
+            Valor:
+            <input
+              type="text"
+              data-testid="value-input"
+              id="value-input"
+              name="valueInput"
+              value={ valueInput }
+              onChange={ this.onChangeInput }
+            />
+          </label>
+          <label htmlFor="description-input">
+            Descrição:
+            <input
+              type="text"
+              data-testid="description-input"
+              id="description-input"
+              name="descriptionInput"
+              value={ descriptionInput }
+              onChange={ this.onChangeInput }
+            />
+          </label>
+          <label htmlFor="currencie-input">
+            Moeda:
+            <select
+              id="currencie-input"
+              name="currencieInput"
+              value={ currencieInput }
+              onChange={ this.onChangeInput }
+            >
+              {
+                currencies.map((currencie) => (
+                  <option key={ currencie }>{ currencie }</option>
+                ))
+              }
+            </select>
+          </label>
+          <label htmlFor="method-input">
+            Forma de pagamento:
+            <select
+              id="method-input"
+              data-testid="method-input"
+              name="methodInput"
+              value={ methodInput }
+              onChange={ this.onChangeInput }
+            >
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag-input">
+            Tag:
+            <select
+              id="tag-input"
+              name="tagInput"
+              data-testid="tag-input"
+              value={ tagInput }
+              onChange={ this.onChangeInput }
+            >
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+        </form>
       </>
     );
   }
@@ -22,6 +113,13 @@ class Wallet extends React.Component {
 
 Wallet.propTypes = {
   dispatch: PropTypes.func.isRequired,
+  currencies: PropTypes.arrayOf(
+    PropTypes.string.isRequired,
+  ).isRequired,
 };
 
-export default connect()(Wallet);
+const mapStateToProps = (state) => ({
+  currencies: state.wallet.currencies,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
